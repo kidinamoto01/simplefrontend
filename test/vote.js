7e14d70793e7ae337eecf983c6e9edb0adf8d652
suyu <kidinamoto@qq.com>
2017-06-19 22:05:02 +0800

    修改测试用例。

--- test/vote.js
+++ test/vote.js
@@ -2,6 +2,7 @@
  * Created by suyu on 17/6/19.
  */
 var vote = artifacts.require("./MyVote.sol");
+
 contract("MyVote", function(accounts) {
     it('should be possible to get Initial sum', function() {
         var voteInstance;
@@ -9,7 +10,7 @@ contract("MyVote", function(accounts) {
             voteInstance = instance;
             return voteInstance.getSum.call();
         }).then(function(sum) {
-            tmpSum= sum;
+            var tmpSum= sum;
             assert.equal(tmpSum,1, "Initial sum should be 1");
             //console.log(result);
         });
@@ -21,7 +22,7 @@ contract("MyVote", function(accounts) {
             voteInstance = instance;
             return voteInstance.getCount.call();
         }).then(function(count) {
-            tmpCount= count;
+            var tmpCount= count;
             assert.equal(tmpCount,0, "Initial Count should be 0");
             //console.log(result);
         });
@@ -38,7 +39,7 @@ contract("MyVote", function(accounts) {
             //console.log(result);
             return  voteInstance.getSum.call();
         }).then(function(sum) {
-            tmpSum= sum;
+            var tmpSum= sum;
             assert.equal(tmpSum,2, "Initial sum should be 2");
             //console.log(result);
         });
@@ -55,14 +56,14 @@ contract("MyVote", function(accounts) {
             //console.log(result);
             return  voteInstance.getSum.call();
         }).then(function(sum) {
-            tmpSum= sum;
+            var tmpSum= sum;
             assert.equal(tmpSum,2, "Initial sum should be 2");
             //console.log(result);
         }).then(function() {
             voteInstance.vote( "Because I have the right to vote", {from: accounts[1]});
             return voteInstance.getCount.call();
         }).then(function(count) {
-            tmpCount= count;
+            var tmpCount= count;
             assert.equal(tmpCount,1, "Initial count should be 1");
         });
     });
@@ -78,19 +79,19 @@ contract("MyVote", function(accounts) {
             //console.log(result);
             return  voteInstance.getSum.call();
         }).then(function(sum) {
-            tmpSum= sum;
+            var tmpSum= sum;
             assert.equal(tmpSum,2, "after giving right to vote, sum should be 2");
 
             voteInstance.vote( "Because I have the right to vote", {from: accounts[1]});
 
             return  voteInstance.getCount.call();
-        }).then(function() {
-            //tmpCount= count;
+        }).then(function(count) {
+            var tmpCount= count;
             assert.equal(tmpCount,1, " Count should be 1");
             voteInstance.vote( "Because I have the right to vote", {from: accounts[1]});
             return  voteInstance.getCount.call();
         }).then(function(count) {
-            tmpSum= count;
+            var tmpSum= count;
             assert.equal(tmpSum,1, "after re-vote, sum should be 1");
         });
     });
@@ -106,7 +107,7 @@ contract("MyVote", function(accounts) {
             // console.log(result);
             return  voteInstance.getSum.call();
         }).then(function(sum) {
-            tmpSum= sum;
+            var tmpSum= sum;
             assert.equal(tmpSum,2, "after giving right to vote, sum should be 2");
             //console.log(result);
         }).then(function() {
@@ -119,7 +120,7 @@ contract("MyVote", function(accounts) {
             //console.log(result);
             return  voteInstance.getSum.call();
         }).then(function(sum) {
-            tmpSum= sum;
+            var tmpSum= sum;
             assert.equal(tmpSum,2, "after giving right to vote, sum should be 2");
         });
     });
@@ -138,7 +139,7 @@ contract("MyVote", function(accounts) {
             //console.log(result);
             return  voteInstance.getSum.call();
         }).then(function(sum) {
-            tmpSum= sum;
+            var tmpSum= sum;
             assert.equal(tmpSum,4, "after giving right to vote, sum should be 4");
             //console.log(result);
         }).then(function() {
@@ -147,7 +148,7 @@ contract("MyVote", function(accounts) {
             voteInstance.vote( "Because I have the right to vote", {from: accounts[3]});
             return  voteInstance.getCount.call();
         }).then(function(count) {
-            tmpSum= count;
+            var tmpSum= count;
             assert.equal(tmpSum,3, "after  vote, count should be 3");
             return voteInstance.isApproved.call();
         }).then(function(result) {
